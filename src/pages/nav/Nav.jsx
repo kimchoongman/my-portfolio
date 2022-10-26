5db8c254a9d2573aa5a1ce8173f0a3a83cc2b02e
choongmankim <cm730712@gmail.com>
2022-10-26 20:34:03 +0900

    Add: Nav 컴포넌트 작업 시작

--- src/pages/nav/Nav.jsx
+++ src/pages/nav/Nav.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <StyledNav>
+      <div>Choongman's PortFolio</div>
+    </StyledNav>
+  );
 };
+
+const StyledNav = styled.nav`
+  padding: 30px;
+  position: fixed;
+  display: flex;
+  border: 1px solid black;
+`;
 
 export default Nav;
