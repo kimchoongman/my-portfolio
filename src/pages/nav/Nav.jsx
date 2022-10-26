8ac869b15a3544e4016a6ccce60b06466867f95b
choongmankim <cm730712@gmail.com>
2022-10-26 21:12:52 +0900

    Add: nav 레이아웃 작업

--- src/pages/nav/Nav.jsx
+++ src/pages/nav/Nav.jsx
@@ -4,16 +4,31 @@ import styled from "styled-components";
 const Nav = () => {
   return (
     <StyledNav>
-      <div>Choongman's PortFolio</div>
+      <div className='title'>Choongman's PortFolio</div>
+      <ul className='nav-bar'>
+        <li className='category'>Intro</li>
+        <li className='category'>Ifo</li>
+        <li className='category'>About</li>
+        <li className='category'>Project</li>
+      </ul>
     </StyledNav>
   );
 };
 
 const StyledNav = styled.nav`
+  width: 100%;
   padding: 30px;
   position: fixed;
   display: flex;
+  justify-content: space-between;
   border: 1px solid black;
+  font-family: "Merriweather Sans", sans-serif;
+
+  .nav-bar {
+    width: 20%;
+    display: flex;
+    justify-content: space-evenly;
+  }
 `;
 
 export default Nav;
--- src/style/GlobalStyle.jsx
+++ src/style/GlobalStyle.jsx
@@ -6,6 +6,7 @@ const GlobalStyle = createGlobalStyle`
     
     *{
         box-sizing: border-box;
+        font-family: 'Merriweather Sans', sans-serif;
     }
 `;
 
